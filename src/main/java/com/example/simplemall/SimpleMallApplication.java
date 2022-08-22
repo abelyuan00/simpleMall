ab54d237483c6d43163d07c41695a2595b7fd06f
abelyuan0822 <abelyuan0822@gmail.com>
2022-08-22 16:00:26 +0800

    Add existing file

---
+++ src/main/java/com/example/simplemall/SimpleMallApplication.java
@@ -0,0 +1,13 @@
+package com.example.simplemall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleMallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleMallApplication.class, args);
+    }
+
+}
