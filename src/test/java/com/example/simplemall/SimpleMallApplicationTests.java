8cacc5259f44d73976e05de58ec89878a9fa29dd
abelyuan0822 <abelyuan0822@gmail.com>
2022-08-22 16:00:26 +0800

    Add existing file

---
+++ src/test/java/com/example/simplemall/SimpleMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simplemall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleMallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
