41ab7a4931a9ef42bdcd43266aadeba09d506069
abelyuan0822 <abelyuan0822@gmail.com>
2022-10-16 16:50:15 +0800

    file delete

---
+++ src/main/java/com/example/simpleMall/Entity/Category.java
@@ -0,0 +1,62 @@
+package com.example.simpleMall.Entity;
+
+import com.sun.istack.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Date;
+
+/**
+ * @author : HaiZhou Yuan
+ * @mailto : abelyuan0822@gmail.com
+ * @created : 10/8/2022, Saturday
+ **/
+
+@Entity
+@Setter
+@Getter
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    /**
+     * 1 shoe - 2 sneaker - 3 nike
+     */
+    @Column
+    @NotNull
+    private Integer categoryLevel = 1;
+
+    /**
+     * id of upper class(shoe - sneaker - Nike)
+     */
+    @Column
+    private Long upperId;
+
+    @Column(unique = true)
+    @NotNull
+    private String name;
+
+    @Column
+    @NotNull
+    private Boolean isDeleted;
+
+    @Column
+    @NotNull
+    private Long creatorId;
+
+    @Column
+    @NotNull
+    private Date creatTime;
+
+    @Column
+    private Long editorId;
+
+    @Column
+    private Date editTime;
+
+
+}
---
+++ src/main/java/com/example/simpleMall/Entity/OrderRecord.java
@@ -0,0 +1,53 @@
+package com.example.simpleMall.Entity;
+
+import com.sun.istack.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Date;
+
+/**
+ * @author : HaiZhou Yuan
+ * @mailto : abelyuan0822@gmail.com
+ * @created : 10/8/2022, Saturday
+ **/
+@Entity
+@Getter
+@Setter
+public class OrderRecord {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column
+    @NotNull
+    private Long customerId;
+
+    @Column
+    @NotNull
+    private Long productId;
+
+    @Column
+    @NotNull
+    private Date orderDate;
+
+    /**
+     * status of order
+     * ORDERED - PAID - WAITING - RECEIVED - COMPLETED - CLOSED
+     */
+    @Column
+    @NotNull
+    private String orderStatus = "ORDERED";
+
+    @Column
+    @NotNull
+    private String paymentInfo;
+
+
+
+
+
+}
