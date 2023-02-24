84e888b4d5ba964c6885d60aebba071cdb109b1f
abelyuan00 <abelyuan0822@gmail.com>
2023-02-24 17:35:45 +0800

    display icon for user

--- src/main/java/com/example/simpleMall/Controller/IndexController.java
+++ src/main/java/com/example/simpleMall/Controller/IndexController.java
@@ -1,10 +1,19 @@
 package com.example.simpleMall.Controller;
 
 
+import com.example.simpleMall.Entity.Admin;
+import com.example.simpleMall.Entity.Customer;
+import com.example.simpleMall.Entity.User;
+import com.example.simpleMall.service.AdminService;
+import com.example.simpleMall.service.CustomerService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpSession;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author : HaiZhou Yuan
@@ -13,6 +22,12 @@ import javax.servlet.http.HttpSession;
  **/
 @Controller
 public class IndexController {
+
+    @Resource
+    AdminService adminService;
+
+    @Resource
+    CustomerService customerService;
 
     @GetMapping({"/index"})
     public String index() {
@@ -37,4 +52,24 @@ public class IndexController {
         return "customer/chartjs";
     }
 
+    @GetMapping ("/getUserIconPath")
+    @ResponseBody
+    public Map<String, String> getUserIconPath(HttpSession session) {
+        Map<String, String> response = new HashMap<>();
+        String iconPath = null;
+        Admin admin = null;
+        User user = null;
+        Long id = (Long)session.getAttribute("adminId");
+        if(null!=session.getAttribute("adminId")) {
+            admin = adminService.loadAdmin(Long.valueOf(id));
+            iconPath = admin.getIconPath();
+        }
+
+        else if (null==session.getAttribute("adminId")&&null!=session.getAttribute("customerId")) {
+            user = customerService.loadCustomer(Long.valueOf((String) session.getAttribute("adminId")));
+            iconPath = user.getIconPath();
+        }
+            response.put("iconPath", iconPath);
+        return response;
+    }
 }
