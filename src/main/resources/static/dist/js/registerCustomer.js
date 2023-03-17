4e12b9cde830cea42187a63c98f58c0ca64c366d
abelyuan00 <abelyuan0822@gmail.com>
2023-03-17 22:07:23 +0800

    customer registration page edit

---
+++ src/main/resources/static/dist/js/registerCustomer.js
@@ -0,0 +1,32 @@
+ function onSubmit(event) {
+    event.preventDefault();
+    const form = document.getElementById('register-form');
+    const passwordInput = document.getElementById('password');
+    const email=document.getElementById('email').value;
+
+    const confirmPasswordInput = document.getElementById('confirm-password');
+    const recaptchaResponse = grecaptcha.getResponse();
+
+     if (!isValidEmail(email)) {
+         alert('Please provide valid email'+email);
+         return;
+     }
+
+    if (passwordInput.value !== confirmPasswordInput.value) {
+    alert('Password and Confirm Password do not match!');
+    return;
+}
+
+    if (!recaptchaResponse) {
+    alert('Please tick the I\'m not a robot!');
+    return;
+}
+    form.submit();
+}
+
+ function isValidEmail(email) {
+     // Regular expression to match email format
+     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+     return emailRegex.test(email);
+ }
+
